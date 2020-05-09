1f03894383c073999f5d816fb82e2c5cab071129
Smit <smitshah95@hotmail.com>
2020-05-09 21:35:35 +0800

    first commit

---
+++ src/main/java/com/spring/utilities/version/VersionApplication.java
@@ -0,0 +1,13 @@
+package com.spring.utilities.version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/utilities/version/VersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.utilities.version;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
